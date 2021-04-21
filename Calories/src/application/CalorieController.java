6140c1771ea6f1b12cee8cca879d469d627e9e2f
victo <victo@DESKTOP-O3VBBV6>
2021-04-21 15:10:50 -0400

    test

---
+++ Calories/src/application/CalorieController.java
@@ -0,0 +1,16 @@
+package application;
+/*
+ * Description : A program that calculates your calorie consumption over a week and compares it to what should be 
+ * attained on average based on your gender, age, weight.
+ * TODO LIST:
+ * - Make it so the programs works and tracks data for different people
+ * - Starting page that lets you choose your profile, if not, 
+ * you make a new one and asks for gender, age, weight and a name that creates a separate profile
+ * - A table with each day of the week, asking for how many calories consumed per meal or in total that day and 
+ * the type of food ate.
+ * - Graph 1 : Calories consumed per day, compared against the average that should be met
+ * - Graph 2 : Distribution of calories per type of food (Meat, Produce, Dairy..etc) in a pie chart
+ */
+public class CalorieController {
+	
+}
